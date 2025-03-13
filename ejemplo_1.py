18ddfa793cbf5806095973f76d6e241f8aadb3e7
Nicolas942 <nicolascabrera@colegioguanenta.edu.co>
2025-03-13 18:26:25 -0500

    actualización

--- ejemplo_1.py
+++ ejemplo_1.py
@@ -18,6 +18,11 @@ class Persona:
 def main():
     p1 = Persona("Nicolás", "Cabrera", 15)
     p1.MostrarPersona()
+    p2 = Persona("Sofía", "Cabrera", 19)
+    p2.MostrarPersona()
+    p1.Edad = 20
+    p1.MostrarPersona()
+
 
 if __name__== "__main__":
     main()
